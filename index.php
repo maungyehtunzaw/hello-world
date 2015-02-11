9edeeb19b4806f0c6fb2343f369eed60e260514e
unknown <yeye@gmail.com>
2015-02-11 16:37:03 +0630

    /this is offline edit

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hello World Index Page....
+<!DOCTYPE html>
+<title>hello World</title>
+	<head>hello Sun</head>
+	<body>
+	
+	<header>Let Dancing </header>
+	<section>Hello Son, Thein!</section>
+	<footer>Do not Trace me! </footer>
+	
+	</body>
+</html>
