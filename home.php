f6fd6f10f9ea6acea402d8a9b714654dd8345e14
Maung Ye Htun Zaw <maungyehtunzaw@users.noreply.github.com>
2015-02-21 17:28:44 +0630

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html charset="UTF-8">
+<body>
+  <h1>I want to back Home........</h1>
+  <?php 
+    for($i=1;i++;i<1500){
+    echo 'i want to back my home';
+    }
+    ?>
+  </body>
+</html>
