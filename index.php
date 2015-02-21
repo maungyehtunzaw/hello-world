5a9e6ddd1f2bdc525192f8fbd9aafa79a3cec3f0
Maung Ye Htun Zaw <maungyehtunzaw@users.noreply.github.com>
2015-02-21 16:43:53 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
+<html charset="UTF-8">
 <title>hello World</title>
 	<head>hello Sun</head>
 	<body>
-	
-	<header>Let Dancing </header>
-	<section>Hello Son, Thein!</section>
-	<aside>Let go</aside>
-	<input type="range" name="apple">
-	<footer>Do not Trace me! </footer>
-	
+		<header>
+			Let Dancing
+		</header>
+		<section>
+			Hello Son, Thein!
+		</section>
+		<aside>Let go</aside>
+		<input type="range" name="apple">
+		<footer>
+			Do not Trace me! 
+		</footer>
 	</body>
-	
+
 </html>
