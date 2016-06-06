6b9a430cf729ee91074d8f32aec973eca0bdb6b2
maungyehtunzaw <maungyehtunzaw@gmail.com>
2016-06-06 16:11:58 +0630

    'mychange'

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html charset="UTF-8">
-<title>hello World</title>
+<title>This is TEST Git</title>
 	<head>hello Sun</head>
 	<body>
 		<header>
@@ -8,9 +8,10 @@
 		</header>
 		<section>
 			Hello Son, Thein!
+			<?php include('home.php'); ?>
 		</section>
 		<aside>Let go</aside>
-		<input type="range" name="apple">
+			<input type="range" name="apple">
 		<footer>
 			Do not Trace me! 
 		</footer>
