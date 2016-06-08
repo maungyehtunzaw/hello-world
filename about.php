7be34e61a0d6552ab0021a9bedf2fc9d75d886bd
Ye <maungyehtunzaw@gmail.com>
2016-06-08 14:03:16 +0630

    more about me;

--- about.php
+++ about.php
@@ -1,8 +1,12 @@
 <Html>
 <head>
-<title>About</title>
+<title>About me</title>
 </head>
 <body>
-  This is about us.
+  <h1>Hi<small>I'm Ye Htun. Work for website Development. Love in design.<small></h1>
+  <p>
+	I want to be a hand that can give to lesser. Not a good talker. Want to be a good doeser.
+	I want to be a brilliant person. Intellignet man. Mature Right
+  </p>
 </body>
 </html>
